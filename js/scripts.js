1135c02ff2a88c447ca01f7ab7e35b511e7ddb90
Bauwens Nicolas <Bauwens Nicolas>
2016-04-13 12:43:02 +0200

    Logic working, needs refactoring and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
-// tutorial1.js
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
-    );
-  }
-});
-
 var TransportGame = React.createClass({
   getInitialState: function() {
     return {
       transport: {},
-      displayedTransport: {}
+      displayedTransport: {},
+      score: 0,
+      total:0
     };
   },
-  componentDidMount: function() {
+  changeHandler: function(win) {
+        if(win){
+          var newScore = this.state.score + 1;
+          this.setState({
+              score: newScore
+          });
+        }
+        var newTotal = this.state.total + 1;
+          this.setState({
+              total: newTotal
+          });
+        this.ajaxRequest();
+  },
+  ajaxRequest: function() {
     this.serverRequest = $.get(this.props.source, function(result) {
       console.log(result);
       this.setState({
@@ -25,15 +29,62 @@ var TransportGame = React.createClass({
       });
     }.bind(this));
   },
+  componentDidMount: function(){
+    this.ajaxRequest();
+  },
   componentWillUnmount: function() {
     this.serverRequest.abort();
   },
   render: function() { 
 		return (
-            <div className="game">
+      <div className="game">
 			 <TransportNumber transport={this.state.displayedTransport}/>
+       <TransportYesNoButtons transport={this.state.transport} displayedTransport={this.state.displayedTransport} onChange={this.changeHandler}/>
+       <div className="score">Score: {this.state.score} sur {this.state.total}</div>
 		  </div>
+
         );
+  }
+});
+
+var TransportYesNoButtons = React.createClass({
+  /*getInitialState: function() {
+    
+  },*/
+  propTypes: {
+        onChange:   React.PropTypes.func
+  },
+  handleClick: function(answer) {
+    console.log(this);
+    var correctColor = this.props.transport.backgroundColor == this.props.displayedTransport.backgroundColor;
+    var win = false;
+    if(answer == true){
+      if(correctColor == true){
+        win = true;
+      }
+    }
+    else{
+      if(correctColor == false){
+        win = true;
+      }
+    }
+
+    if(typeof this.props.onChange === 'function') {
+      this.props.onChange(win);
+    }
+    
+  },
+  render: function() {
+    return (
+      <div>
+        <button onClick={this.handleClick.bind(this, true)}>
+          Oui
+        </button>
+        <button onClick={this.handleClick.bind(this, false)}>
+          Non
+        </button>
+      </div>
+    );
   }
 });
 
